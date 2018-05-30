494582ee907f2e54d093e424824dcd23cce94ec4
김정환 <6pack@woowahan.com>
2018-05-30 12:19:45 +0900

    watch를 이용해 구현

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,47 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <form @submit.prevent="onSubmit">
+      <input v-model="name">
+      <p v-if="errorBag.name">{{errorBag.name[0]}}</p>
+      <button type="submit">Submit</button>
+    </form>
   </div>
 </template>
 
 <script>
+
+const Validator = {
+  validate (val) {
+    const errors = []
+    
+    if (!val) {
+      errors.push('name field is required')
+    } else {
+      if (val.length < 3) errors.push('name filed should have length of 3')
+    }
+    
+    return errors
+  }
+}
+
 export default {
-  name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      name: '',
+      errorBag: {
+        name: []
+      }
+    }
+  },
+  watch: {
+    name (val) {
+      this.errorBag.name = Validator.validate(val)
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.errorBag.name = Validator.validate(this.name)
     }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>
